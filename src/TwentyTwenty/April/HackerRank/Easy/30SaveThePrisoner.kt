37f61827c306261bbd7a74542d664f5eaa0c7570
Tae Soo Kim <taesoo.kim0412@gmail.com>
2020-04-24 17:01:25 -0700

    modulus, math, upper limit index, & everything in between.

--- src/TwentyTwenty/April/HackerRank/Easy/30SaveThePrisoner.kt
+++ src/TwentyTwenty/April/HackerRank/Easy/30SaveThePrisoner.kt
@@ -1,28 +1,59 @@
 package TwentyTwenty.April.HackerRank.Easy
 
 
-//04-23-2020
-//timeout
-//test cases work
+// the power of math
 //https://www.hackerrank.com/challenges/save-the-prisoner/problem
 
 fun saveThePrisoner(n: Int, m: Int, s: Int): Int {
-    return recThePrisoner(n, m, s, s)
+    // Num prisoners: n
+    // Num sweets: m
+    // Chair number to start: s
+
+    // 5000 sweets 1000 prisoners: start at 3
+    // return Remainder of sweets/prisoners + 3
+
+    // 1000 sweets 5000 prisoners: start at 3
+    // return sweets + 3
+    if (n > m) return m % n + s - 1
+    else if (n == m) return s
+
+
+
+    // if candies > prisoners
+    /*else if (n < m){
+        var nDouble = n.toDouble()
+        var mDouble = m.toDouble()
+        while (mDouble > nDouble){
+            mDouble -= nDouble
+        }
+        return (mDouble + s - 1).toInt()
+    }*/
+    val idx = (s - 1 + m ) % n
+    return if (idx == 0) n else idx
 }
 
-tailrec fun recThePrisoner(n: Int, m: Int, s: Int,
+
+/*tailrec fun recThePrisoner(n: Int, m: Int, s: Int,
                    prisonerIndx:Int): Int {
     if (m==1) return prisonerIndx
     var prisonerIndx = prisonerIndx
         prisonerIndx = if (prisonerIndx + 1 > n) 1 else prisonerIndx + 1
     return recThePrisoner(n, m-1, s, prisonerIndx)
-}
+}*/
 
 
 fun main(args: Array<String>) {
     //{Prisoners, Sweets, Initial}
+    //6
     println(saveThePrisoner(7, 19, 2))
+    //3
     println(saveThePrisoner(3, 7, 3))
+    //2
+    println(saveThePrisoner(5, 2, 1))
+    //3
+    println(saveThePrisoner(5, 2, 2))
+    //4
+    println(saveThePrisoner(4, 2, 3))
     //356482915
     println(saveThePrisoner(715950220, 876882456, 195550680))
 }
---
+++ src/TwentyTwenty/April/HackerRank/Easy/31CircularArrayRotation.kt
@@ -0,0 +1,33 @@
+package TwentyTwenty.April.HackerRank.Easy
+
+import TwentyTwenty.April.HackerRank.Library.Library
+import java.util.*
+
+
+//20/20
+//Omg it passed
+//https://www.hackerrank.com/challenges/circular-array-rotation
+//04-24-2020
+
+fun circularArrayRotation(a: Array<Int>, k: Int, queries: Array<Int>): Array<Int> {
+    var k = k
+    var newArray = Array<Int>(a.size){0}
+    k = k % a.size
+    var upperLimit = newArray.size-k
+    for (i in 0..upperLimit - 1){
+        newArray[k + i] = a[i]
+    }
+    for (i in upperLimit..a.size-1){
+        newArray[i - upperLimit] = a[i]
+    }
+    for (i in 0..queries.size-1){
+        queries[i] = newArray[queries[i]]
+    }
+    return queries
+}
+
+fun main(args: Array<String>) {
+    //2, 3, 1
+    println(Arrays.toString(circularArrayRotation(arrayOf(1, 2, 3), 2, arrayOf(0, 1, 2) )))
+    println(Arrays.toString(circularArrayRotation(Library.arrayify("39356 87674 16667 54260 43958 70429 53682 6169 87496 66190 90286 4912 44792 65142 36183 43856 77633 38902 1407 88185 80399 72940 97555 23941 96271 49288 27021 32032 75662 69161 33581 15017 56835 66599 69277 17144 37027 39310 23312 24523 5499 13597 45786 66642 95090 98320 26849 72722 37221 28255 60906"), 51, arrayOf(47, 10))))
+}
