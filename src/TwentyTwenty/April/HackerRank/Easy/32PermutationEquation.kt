3d39af7c8ec994baff7744ad873ee15b45a5db7a
Tae Soo Kim <taesoo.kim0412@gmail.com>
2020-04-26 07:58:59 -0700

    I'm studying number theory while taking calculus and I don't even major in math while I'm developing software while studying computer science for a job

---
+++ src/TwentyTwenty/April/HackerRank/Easy/32PermutationEquation.kt
@@ -0,0 +1,21 @@
+package TwentyTwenty.April.HackerRank.Easy
+
+import TwentyTwenty.April.HackerRank.Library.Library
+import java.util.*
+
+
+
+//fun main(args: Array<String>) {
+//    readLine()
+//    val pairs = readLine()!!.split(' ')
+//            .withIndex()
+//            .map { it.value.toInt() - 1 }
+//    val arr: Array<Int> = pairs
+//            .foldIndexed(Array(pairs.size) { 0 }) { i, a: Array<Int>, v ->
+//                a[v] = i
+//                return@foldIndexed a
+//            }
+//    for (x in 0..pairs.size - 1) {
+//        println(arr[arr[x]] + 1)
+//    }
+//}
