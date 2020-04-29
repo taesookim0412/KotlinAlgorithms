385385e39bd0e4ed365b456dc5397366ab2f410c
Tae Soo Kim <taesoo.kim0412@gmail.com>
2020-04-28 19:26:43 -0700

    널 위해 예쁜 거짓을 빚어내

--- src/TwentyTwenty/April/HackerRank/Easy/32PermutationEquation.kt
+++ src/TwentyTwenty/April/HackerRank/Easy/32PermutationEquation.kt
@@ -4,18 +4,31 @@ import TwentyTwenty.April.HackerRank.Library.Library
 import java.util.*
 
 
+//https://www.hackerrank.com/challenges/permutation-equation/problem
+//04-28-2020
+//20/20
 
-//fun main(args: Array<String>) {
-//    readLine()
-//    val pairs = readLine()!!.split(' ')
-//            .withIndex()
-//            .map { it.value.toInt() - 1 }
-//    val arr: Array<Int> = pairs
-//            .foldIndexed(Array(pairs.size) { 0 }) { i, a: Array<Int>, v ->
-//                a[v] = i
-//                return@foldIndexed a
-//            }
-//    for (x in 0..pairs.size - 1) {
-//        println(arr[arr[x]] + 1)
-//    }
-//}
+fun permutationEquation(p: Array<Int>): Array<Int> {
+    var numsIdxs = Array<Int>(p.size) { 0 }
+
+    //Pair indexes with a seperate array of nums
+    p.mapIndexed { idx, num ->
+        numsIdxs[num-1] = idx
+    }
+
+    var newArr: Array<Int> = numsIdxs.foldIndexed(Array<Int>(p.size) { 0 }) { i, a: Array<Int>, num ->
+        a[num] = p[i]
+        return@foldIndexed a
+    }
+    var newP: Array<Int> = Array<Int>(p.size) {0}
+    for (i in 0..newP.size-1){
+        newP[i] = numsIdxs[numsIdxs[i]] + 1
+    }
+    return newP
+}
+
+
+fun main(args: Array<String>) {
+    println(Arrays.toString(permutationEquation(arrayOf(2, 3, 1))))
+    println(Arrays.toString(permutationEquation(arrayOf(1, 3, 5, 1, 2))))
+}
