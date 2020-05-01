76dc9a497ec3d244e96529b52e7234c0c314fa19
Tae Soo Kim <taesoo.kim0412@gmail.com>
2020-04-30 18:33:57 -0700

    I'm all alone. . . . Major Lazer - Cold Water (feat. Justin Bieber & MØ)

--- src/TwentyTwenty/April/HackerRank/Easy/34FindDigits.kt
+++ src/TwentyTwenty/April/HackerRank/Easy/34FindDigits.kt
@@ -1,19 +1,22 @@
 package TwentyTwenty.April.HackerRank.Easy
 
 
+//04-30-2020
 //https://www.hackerrank.com/challenges/find-digits/problem
+//25/25
 fun findDigits(n: Int): Int {
-    var idx = 10
-    do{
-        //??
-        var num = (n%idx) % idx
-        println(num)
-        idx *= 10
-    }while(idx < n)
-    return 929213
+    var ghostNum = n
+    var sum = 0
+    while (ghostNum > 0) {
+        var num = ghostNum % 10
+        ghostNum /= 10
+        if (num != 0) if (n % num == 0) sum++
+    }
+    return sum
 }
 
 fun main(args: Array<String>) {
     println(findDigits(2))
+    println(findDigits(12))
     println(findDigits(1012))
 }
